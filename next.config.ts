f8adf587ce8c6658b15ad4e1d1086a9af1b53b78
Ayesha Zafar <Ayeshazafar594@gmail.com>
2026-02-20 23:37:49 +0500

    Added deployment config

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  basePath: "/portfolio",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
